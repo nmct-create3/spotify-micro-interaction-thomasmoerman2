77fae217ebd2c8ec4cd1494d5bc09222595ec155
MeanRom <moer.thomas10@gmail.com>
2024-11-15 10:41:34 +0100

    setup

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import path from "path";
+
+export default defineConfig({
+  // Configuration options go here
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
+  },
+  plugins: [
+    // Add your plugins here
+  ],
+});
